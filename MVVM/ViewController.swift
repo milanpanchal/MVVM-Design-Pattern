099566e317b259592e9ee88c3eb356f3259432e8
Milan Panchal <sam07it22@gmail.com>
2020-03-18 23:10:06 +0530

    Initial Commit

---
+++ MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM
+//
+//  Created by Milan Panchal on 18/03/20.
+//  Copyright © 2020 Jeenal Infotech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
