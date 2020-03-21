eb3f17fb0d893888a99737eedc8c39c440f9da39
Milan Panchal <sam07it22@gmail.com>
2020-03-21 12:53:41 +0530

    feat: Test Case for UserViewMode

--- MVVMTests/MVVMTests.swift
+++ MVVMTests/MVVMTests.swift
@@ -19,16 +19,24 @@ class MVVMTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testUserViewModel() {
+        
+        let name = Name.init(title: "Mr.", first: "Milan", last: "Panchal")
+        let picture = Picture.init(large: "", medium: "", thumbnail: "https://avatars3.githubusercontent.com/u/2879673?s=60&u=8fd05ac67922b601e84f9c55e011307547dee445&v=4")
+        let user = User.init(gender: "Male", name: name, email: "test@gmail.com", picture: picture, nat: "IN", cell: "123456789", phone: "9898989898")
+        
+        let userViewModel = UserViewModel(user: user)
+        
+        // Check for Nil or empty string URL
+        XCTAssertNotNil(user.picture.thumbnail)
+        XCTAssertNotEqual(user.picture.thumbnail, "")
+        
+        // Check for valid URL
+        XCTAssertNotNil(userViewModel.thumbnailImageUrl)
+        
+        XCTAssertEqual([user.name.title, user.name.first, user.name.last].joined(separator: " "), userViewModel.fullName)
+        XCTAssertEqual([user.cell, user.phone].joined(separator: " / "), userViewModel.contactNumber)
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
